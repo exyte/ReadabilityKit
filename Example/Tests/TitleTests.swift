b8aa5f1eed47487bfb23875e1827702c123e0066
Victor Sukochev <sukochevvv@gmail.com>
2016-06-27 11:37:10 +0600

    Title tests

---
+++ Example/Tests/TitleTests.swift
@@ -0,0 +1,104 @@
+//
+//  TitleTests.swift
+//  ReadabilityKit
+//
+//  Copyright (c) 2016 Exyte http://www.exyte.com
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import XCTest
+import ReadabilityKit
+
+class TitleTests: XCTestCase {
+
+	override func setUp() {
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+	}
+
+	func testTitleCase1() {
+
+		let content = "<html><head><title>Test<title><head><html>"
+		guard let contentData = content.dataUsingEncoding(NSUTF8StringEncoding) else {
+			return
+		}
+
+		let parser = Readability(data: contentData)
+		guard let title = parser.title() else {
+			XCTFail("Title parsing failed.")
+			return
+		}
+
+		XCTAssert(title == "Test", "Title parsing failed.")
+	}
+
+	func testTitleCase2() {
+
+		let content = "<html><head><meta name = \"title\" content = \"Test\"><head><html>"
+		guard let contentData = content.dataUsingEncoding(NSUTF8StringEncoding) else {
+			return
+		}
+
+		let parser = Readability(data: contentData)
+		guard let title = parser.title() else {
+			XCTFail("Title parsing failed.")
+			return
+		}
+
+		XCTAssert(title == "Test", "Title parsing failed.")
+	}
+
+	func testTitleCase3() {
+
+		let content = "<html><head><meta property = \"og:title\" content = \"Test\"><head><html>"
+		guard let contentData = content.dataUsingEncoding(NSUTF8StringEncoding) else {
+			return
+		}
+
+		let parser = Readability(data: contentData)
+		guard let title = parser.title() else {
+			XCTFail("Title parsing failed.")
+			return
+		}
+
+		XCTAssert(title == "Test", "Title parsing failed.")
+	}
+
+	func testTitleCase4() {
+
+		let content = "<html><head><meta name = \"twitter:title\" content = \"Test\"><head><html>"
+		guard let contentData = content.dataUsingEncoding(NSUTF8StringEncoding) else {
+			return
+		}
+
+		let parser = Readability(data: contentData)
+		guard let title = parser.title() else {
+			XCTFail("Title parsing failed.")
+			return
+		}
+
+		XCTAssert(title == "Test", "Title parsing failed.")
+	}
+}
