9f4a5b50e3629e9d5f407aac706466735f85d87b
Victor Sukochev <sukochevvv@gmail.com>
2016-08-16 18:11:15 +0600

    Separate project for Carthage support

---
+++ Dependencies/iOS/Ji.framework/Headers/Ji.h
@@ -0,0 +1,62 @@
+//
+//  Ji.h
+//  Ji
+//
+//  Created by Honghao Zhang on 2015-08-09.
+//  Copyright (c) 2015 Honghao Zhang (张宏昊)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Ji.
+FOUNDATION_EXPORT double JiVersionNumber;
+
+//! Project version string for Ji.
+FOUNDATION_EXPORT const unsigned char JiVersionString[];
+
+#import "tree.h"
+#import "parser.h"
+#import "HTMLtree.h"
+#import "HTMLparser.h"
+#import "xpath.h"
+#import "xpathInternals.h"
+#import "xmlerror.h"
+
+#import "c14n.h"
+#import "catalog.h"
+#import "chvalid.h"
+#import "debugXML.h"
+#import "nanoftp.h"
+#import "nanohttp.h"
+#import "parserInternals.h"
+#import "pattern.h"
+#import "relaxng.h"
+#import "schemasInternals.h"
+#import "schematron.h"
+#import "uri.h"
+#import "xinclude.h"
+#import "xmlmodule.h"
+#import "xmlreader.h"
+#import "xmlsave.h"
+#import "xmlschemas.h"
+#import "xmlschemastypes.h"
+#import "xmlunicode.h"
+#import "xmlwriter.h"
+#import "xpointer.h"
--- ReadabilityKit.podspec
+++ ReadabilityKit.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'ReadabilityKit'
-  s.version          = '0.4.2'
+  s.version          = '0.4.3'
   s.summary          = 'Metadata extractor for news, articles and full-texts.'
   
   s.homepage         = 'https://github.com/exyte/ReadabilityKit.git'
@@ -12,7 +12,7 @@ Pod::Spec.new do |s|
   s.ios.deployment_target = '8.0'
   s.osx.deployment_target = "10.9"
 
-  s.source_files = 'ReadabilityKit/Classes/**/*'
+  s.source_files = 'Sources/*.swift'
 
   s.xcconfig = { "HEADER_SEARCH_PATHS" => "$(SDKROOT)/usr/include/libxml2" }
   s.dependency 'Ji', '~> 1.2.0'
